7e7e964e18f20e3eb77e965761672c789eae13a8
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-09-21 10:42:25 -0400

    Try FelixCloud S3

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM quay.io/domino/python-public:3.9.6-slim
-RUN pip install --no-cache psycopg2-binary
-RUN pip install --no-cache mlflow
+RUN pip install --no-cache psycopg2-binary boto3 mlflow
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export AWS_ACCESS_KEY_ID=${FELIX_AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${FELIX_AWS_SECRET_ACCESS_KEY}
 mlflow server --host 0.0.0.0 --port ${PORT} --backend-store-uri ${DATABASE_URL/postgres/postgresql} --default-artifact-root file:///root/mlflow
